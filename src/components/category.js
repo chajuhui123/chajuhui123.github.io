8b694ff0a360143d2907005726b575f5277955c5
chajuhui123 <chajuhui123@naver.com>
2023-03-12 10:55:04 +0900

    [feat] state 방식의 category 선택 기능 연동

--- src/components/category.js
+++ src/components/category.js
@@ -1,23 +1,27 @@
 import * as React from "react";
 
-const Category = ({ categories }) => {
-  const selectedCategory = "ALL";
+const Category = ({ categories, selectedCategory, handleChangeCategory }) => {
   return (
     <ul className="category-wrapper">
-      <CategoryItem selectedCategory={selectedCategory} category="ALL" />
+      <CategoryItem
+        selectedCategory={selectedCategory}
+        category="ALL"
+        onClick={() => handleChangeCategory("ALL")}
+      />
 
       {categories.map((category, idx) => (
         <CategoryItem
           key={idx}
           selectedCategory={selectedCategory}
           category={category}
+          onClick={() => handleChangeCategory(category)}
         />
       ))}
     </ul>
   );
 };
 
-const CategoryItem = ({ selectedCategory, category }) => {
+const CategoryItem = ({ selectedCategory, category, onClick }) => {
   return (
     <div
       className={
@@ -25,6 +29,7 @@ const CategoryItem = ({ selectedCategory, category }) => {
           ? "category-item selected"
           : "category-item non-selected"
       }
+      onClick={onClick}
     >
       <li>{category}</li>
     </div>
