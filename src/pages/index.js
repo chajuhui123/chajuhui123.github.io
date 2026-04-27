10134ef618015d6910f6f79b1517b88263b36200
chajuhui123 <chajuhui123@naver.com>
2026-04-27 19:11:43 +0900

    docs: [260427] E2E 자동화

--- src/pages/index.js
+++ src/pages/index.js
@@ -111,7 +111,7 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }, filter: { frontmatter: { draft: { ne: true } } }) {
       nodes {
         excerpt
         fields {
